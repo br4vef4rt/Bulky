28dcc867f6120635827c4aebd984f231f57cfca7
DZ <dariusz.zgorzelski@gmail.com>
2023-12-21 18:29:50 +0100

    1

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,9 +1,19 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "Range must be between 1 and 100")]
         public int DisplayOrder { get; set; }
+        
     }
 }
